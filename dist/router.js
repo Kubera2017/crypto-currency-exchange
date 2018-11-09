aea3d2d454154095ecac778a4ee593376495b695
Alex <alexey.kum@gmail.com>
2018-11-09 20:41:29 +0400

    Final

--- dist/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var ExchangeAutomaton = require("./controllers/automaton.controller");
-var router = express_1.Router();
-router.get('/', ExchangeAutomaton.getState);
-router.post('/withdraw', [ExchangeAutomaton.check, ExchangeAutomaton.withdraw]);
-exports.AutomatonRouter = router;
